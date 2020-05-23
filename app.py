f7903185e94e1613bee1f96d43a006570512d1b8
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2020-05-23 17:07:36 -0600

    use local dockerfile

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import os
 
-from aws_cdk import core
+from aws_cdk import core, aws_ecs
 from airflow_cdk import FargateAirflow
 
 
 app = core.App()
 
-FargateAirflow(app, "airflow-cdk")
+FargateAirflow(
+    app, "airflow-cdk", base_image=aws_ecs.ContainerImage.from_asset(".")
+)
 
 app.synth()
