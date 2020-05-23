ffe4dc34b658bd29c19632db9c1f5c92ec6df15e
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2020-05-23 17:08:04 -0600

    add airflow to dev dependencies

--- setup.py
+++ setup.py
@@ -23,7 +23,17 @@ setuptools.setup(
         "aws-cdk.aws_s3>=1.34.1",
         "aws-cdk.aws_elasticloadbalancingv2>=1.34.1",
     ],
-    extras_require={"dev": ["pytest", "toml", "black", "twine", "invoke"]},
+    extras_require={
+        "dev": [
+            "pytest",
+            "toml",
+            "black",
+            "twine",
+            "invoke",
+            "jinja2",
+            "apache-airflow[postgres,celery,aws]>=1.10.10",
+        ]
+    },
     python_requires=">=3.6",
     classifiers=[
         "Development Status :: 4 - Beta",
